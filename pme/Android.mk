bd5388e75d2786e0e0cb4f7dd884fa9ad99fe326
fagyi <fagyipda@gmail.com>
2017-07-04 15:10:31 +0200

    Revert "pme: Fix HotwordEnrollment apk inclusion"
    This reverts commit c0a5d3eebd5707e75e94f58b991675da4bd1a0d3.

--- pme/Android.mk
+++ pme/Android.mk
@@ -167,16 +167,5 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_SUFFIX := .xml
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := HotwordEnrollment
-LOCAL_MODULE_OWNER := htc
-LOCAL_SRC_FILES := proprietary/priv-app/HotwordEnrollment/HotwordEnrollment.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
 
 endif
