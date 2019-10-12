8745fa7c0d28f4c067bb8b3bfa3f2c6b6f2bf31a
Fagyi <fagyipda@gmail.com>
2019-10-12 16:33:09 +0200

    pme: Move WfdCommon to soong

--- pme/Android.mk
+++ pme/Android.mk
@@ -32,16 +32,6 @@ LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := WfdCommon
-LOCAL_MODULE_OWNER := htc
-LOCAL_SRC_FILES := proprietary/framework/WfdCommon.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := ims
 LOCAL_MODULE_OWNER := htc
 LOCAL_SRC_FILES := proprietary/priv-app/ims/ims.apk
